04b033d6de07c190224cc54999a8f877307ca2d5
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-05-25 17:59:30 -0300

    06-navigation

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,21 @@
+import { useRouter } from 'next/router'
+
 const ContactPage = () => {
+    const router = useRouter();
+
+    const redirectUser = () => {
+        router.push({
+            pathname:'/wheels/[id]/[color]/[type]',
+            query:{id:1,color:'red',type:'round'}
+          })
+    }
+
     return(
         <>
             <h1>contact</h1>
+            <button onClick={redirectUser}>
+                Go to home
+            </button>
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
+import Link from 'next/link';
+
 
 const Home = () => {
   return (
     <>
       <h1>Honey i am home</h1>
+      <ul>
+        <li>
+          {/* <a href="/users">User</a> */}
+          <Link href="/users/1">
+            <a className="styleLink">Users</a>
+          </Link>
+        </li>
+        <li>
+          {/* <a href="/users">User</a> */}
+          <Link href={{
+            pathname:'/wheels/[id]/[color]/[type]',
+            query:{id:1,color:'red',type:'round'}
+          }}>Go to wheels</Link>
+        </li>
+      </ul>
     </>
   )
 }
