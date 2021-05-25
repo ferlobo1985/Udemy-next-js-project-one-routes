1357047cd96a3a26f86a47705574a20eb3131c1a
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-05-25 18:48:52 -0300

    09-meta tags and head

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,17 @@
 import Redirect from '../components/redirect';
+import Head from 'next/head';
 
 const ContactPage = () => {
-
+    const title = 'Contact us';
 
     return(
         <>
+            <Head>
+                <title>{title}</title>
+                <meta name="description" content="Contact us, we will not reply"/>
+                <meta property="og:title" content="My page title" key="title"/>
+            </Head>
+
             <h1>contact</h1>
             <Redirect/>
         </>
